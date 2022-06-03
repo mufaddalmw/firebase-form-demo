ee06230a570a8279c53867a1c68f9bc58d252e0f
Mufaddal shabbir Husan <mufaddalshabbir.husan@dnata.com>
2022-06-03 15:55:19 +0400

    added firebase init

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
 import '../styles/globals.css'
 
+// add firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCh7SAOjgM8oIJUnzk4nXx4Sv-0Gcnsd5k",
+  authDomain: "fir-form-demo-9e4f3.firebaseapp.com",
+  projectId: "fir-form-demo-9e4f3",
+  storageBucket: "fir-form-demo-9e4f3.appspot.com",
+  messagingSenderId: "43946527156",
+  appId: "1:43946527156:web:20a3a4088b1f8fa22b0ae1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
