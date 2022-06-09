e3455f73b1f96f59d4b255de135ffa04e8305ee3
Mufaddal shabbir Husan <mufaddalshabbir.husan@dnata.com>
2022-06-09 16:29:15 +0400

    updates in public runtime config variables, added logic for custom form validation

--- pages/_app.js
+++ pages/_app.js
@@ -10,19 +10,17 @@ const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
 // Will only be available on the server-side
 
 // Will be available on both server-side and client-side
-console.log(publicRuntimeConfig.APIKEY)
-
 // TODO: Add SDKs for Firebase products that you want to use
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: publicRuntimeConfig.APIKEY,
-  authDomain: "fir-form-demo-9e4f3.firebaseapp.com",
-  databaseURL: "https://fir-form-demo-9e4f3-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "fir-form-demo-9e4f3",
-  storageBucket: "fir-form-demo-9e4f3.appspot.com",
-  messagingSenderId: "43946527156",
-  appId: "1:43946527156:web:20a3a4088b1f8fa22b0ae1"
+  authDomain: publicRuntimeConfig.AUTHDOMAIN,
+  databaseURL: publicRuntimeConfig.DATABASEURL,
+  projectId: publicRuntimeConfig.PROJECTID,
+  storageBucket: publicRuntimeConfig.STORAGEBUCKET,
+  messagingSenderId: publicRuntimeConfig.MESSAGINGSENDERID,
+  appId: publicRuntimeConfig.APPID
 };
 
 // Initialize Firebase
--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,10 @@ import { getDatabase, ref, set } from "firebase/database";
 export default function Home() {
   const [inputs, setInputs] = useState({});
   const [successmsg, setsuccessmsg] = useState(null);
-
+  
   // input change event
   const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
+    const { name, value } = event.target;
     setInputs(values => ({...values, [name]: value}))
     if (successmsg) {
       setsuccessmsg(null);
@@ -18,12 +17,34 @@ export default function Home() {
   // submit button event
   const handleSubmit = (event) => {
     event.preventDefault();
+    const data = {...inputs, date: new Date().toString()};
     
-    writeUserData({...inputs, date: new Date().toString()});
+    /* check for validation */
+    let formValid = false;
+    if (data?.fullname && data?.email && data?.subject && data?.message && data?.date) {
+      formValid = true;
+    } 
+    if (!data?.fullname) {
+      setInputs(values => ({...values, fullname: ''}))
+    } 
+    if (!data?.email) {
+      setInputs(values => ({...values, email: ''}))
+    }
+    if (!data?.subject) {
+      setInputs(values => ({...values, subject: ''}))
+    }
+    if (!data?.message) {
+      setInputs(values => ({...values, message: ''}))
+    }
+
+    if (formValid) {
+      writeUserData(data);
+    } else {
+      setsuccessmsg(false);
+    }
   }
 
   const writeUserData = (data) => {
-    console.log(data);
     const db = getDatabase();
     set(
       ref(db, 'query/' + Date.now()), 
@@ -40,8 +61,6 @@ export default function Home() {
       console.log('// The write failed...', error);
       setsuccessmsg(false);
     });
-
-    
   }
 
   return (
@@ -57,25 +76,30 @@ export default function Home() {
               <label htmlFor="name" className="mb-3 block text-base font-medium text-[#07074D]">
                 Full Name
               </label>
-              <input type="text" name="fullname" id="name" placeholder="Full Name" value={inputs.fullname || ""} onChange={handleChange} className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+              <input type="text" name="fullname" id="name" placeholder="Full Name" value={inputs?.fullname || ""} onChange={handleChange} onBlur={handleChange} className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+              {inputs?.fullname === '' && <span className="text-red-700 text-xs">Full Name field is required</span>}
+              
             </div>
             <div className="mb-5">
               <label htmlFor="email" className="mb-3 block text-base font-medium text-[#07074D]">
                 Email Address
               </label>
-              <input type="email" name="email" id="email" placeholder="example@domain.com" value={inputs.email || ""} onChange={handleChange} className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+              <input type="email" name="email" id="email" placeholder="example@domain.com" value={inputs?.email || ""} onChange={handleChange} onBlur={handleChange} className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+              {inputs?.email === '' && <span className="text-red-700 text-xs">Email Address field is required</span>}
             </div>
             <div className="mb-5">
               <label htmlFor="subject" className="mb-3 block text-base font-medium text-[#07074D]">
                 Subject
               </label>
-              <input type="text" name="subject" id="subject" placeholder="Enter your subject" value={inputs.subject || ""} onChange={handleChange} className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+              <input type="text" name="subject" id="subject" placeholder="Enter your subject" value={inputs?.subject || ""} onChange={handleChange} onBlur={handleChange} className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+              {inputs?.subject === '' && <span className="text-red-700 text-xs">Subject field is required</span>}
             </div>
             <div className="mb-5">
               <label htmlFor="message" className="mb-3 block text-base font-medium text-[#07074D]">
                 Message
               </label>
-              <textarea rows={4} name="message" id="message" placeholder="Type your message" value={inputs.message || ""} onChange={handleChange} className="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+              <textarea rows={4} name="message" id="message" placeholder="Type your message" value={inputs?.message || ""} onChange={handleChange} onBlur={handleChange} className="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+              {inputs?.message === '' && <span className="text-red-700 text-xs">Message field is required</span>}
             </div>
             <div>
               <button className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none">
@@ -92,20 +116,20 @@ export default function Home() {
               </svg>
               <div className="text-green-700">
                 <div className="font-bold text-xl">Your query is submitted succcessfully!</div>
-                <div>We will get back to shortly.</div>
+                <div>We will get back to you shortly.</div>
               </div>
             </div>
 
             {/* Error message */}
             <div className={`flex gap-4 bg-red-100 p-4 rounded-md mt-3 ${successmsg === false ? '' : 'hidden'}`}>
               <div className="w-max">
-                <div className="h-10 w-10 flex rounded-full bg-gradient-to-b from-red-100 to-red-300 text-red-700 inline-flex items-center justify-center">
+                <div className="h-10 w-10 rounded-full bg-gradient-to-b from-red-100 to-red-300 text-red-700 inline-flex items-center justify-center">
                   <svg width={24} height={24} xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-red-700"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm0 10.293l5.293-5.293.707.707-5.293 5.293 5.293 5.293-.707.707-5.293-5.293-5.293 5.293-.707-.707 5.293-5.293-5.293-5.293.707-.707 5.293 5.293z" /></svg>
                 </div>
               </div>
               <div className="space-y-1 text-sm">
                 <h6 className="font-medium text-red-900">Permission denied</h6>
-                <p className="text-red-700 leading-tight"> - please complete all the fields above</p>
+                <p className="text-red-700 leading-tight">Please complete all the fields above</p>
               </div>
             </div>
           </form>
